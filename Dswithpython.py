59f5fc53399820f088684de8145041c6d4a4d075
urlifemy <cogs132@gmail.com>
2022-07-09 23:32:20 +0530

    Create Dswithpython.py
    "To do list"

---
+++ Dswithpython.py
@@ -0,0 +1,27 @@
+# This py script has All the DataStructures and their Implementaions
+#           DataStructures Helps developer TO Structure and Orginize the data
+#     -->>     1. Linked list
+#     -->>     2. Doubly Linked List
+#     -->>     3. Circular Linked List
+#     -->>     4. Stack
+#     -->>     5. Queue
+#     -->>     6. Binary Search Tree
+#     -->>     7. Graphs
+#     -->>     8. Binary Heaps
+
+#------------------------- Advance Data Strutures -----------------------------------
+#     -->>     9. Circular Queue
+#     -->>    10. Priority Queue
+#     -->>    11. Deque
+#     -->>    12. Self Balancing BSTs
+#     -->>    13. AVL Tree
+#     -->>    14. Red Black Tree
+#     -->>    15. Segment Tree
+#     -->>    16. Trie
+#     -->>    17. Binary Indexed Tree or Fenwick Tree
+#     -->>    18. Suffix Array
+#     -->>    19. K Dimensional Tree
+#     -->>    20. N-array Trees
+#     -->>    21. Disjoint Set
+#     -->>    22. Spanning Tree
+# new
